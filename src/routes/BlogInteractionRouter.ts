97a91e0a6ac22660d83411bff693aaa4aba25eb4
Eben's <mac@MacBook-Air.local>
2025-12-30 08:30:07 +0100

    updated

--- src/routes/BlogInteractionRouter.ts
+++ src/routes/BlogInteractionRouter.ts
@@ -13,19 +13,18 @@ import {
 
 const commentRouter = express.Router();
 
-// Like routes
 commentRouter.post('/blogs/:id/like', likeBlogPost);
 commentRouter.post('/blogs/:id/unlike', unlikeBlogPost);
 commentRouter.get('/blogs/:id/check-like', checkUserLike);
 commentRouter.get('/blogs/:id/like-count', getLikeCount);  
-commentRouter.get('/blogs/:id/comment-count', getCommentCount); 
 commentRouter.get('/blogs/:id/check-like-with-count', checkUserLikeWithCount); 
 
-// Comment routes
+
 commentRouter.post('/blogs/:id/addcomments', addComment);
 commentRouter.get('/blogs/:id/getcomments', getComments);
+commentRouter.get('/blogs/:id/comment-count', getCommentCount); 
 
-// comments by username
+
 commentRouter.get("/comment/user/:username", getCommentsByUsername)
 
 export default commentRouter;
